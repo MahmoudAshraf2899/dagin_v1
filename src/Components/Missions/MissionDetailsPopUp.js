08cfdf04376ebe19e26007f6bebb7c655845b107
MahmoudAshraf2899 <mahmodashrf79@gmail.com>
2023-11-18 14:54:07 +0200

    Build Missions types and start to build Mission Details popup

---
+++ src/Components/Missions/MissionDetailsPopUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MissionDetailsPopUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {}
+  render() {
+    return <div></div>;
+  }
+}
+export default MissionDetailsPopUp;
